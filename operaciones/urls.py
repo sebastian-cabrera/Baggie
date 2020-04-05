295c766bd4211521220be5f5b62bb5ad84f63c20
sebastian-cabrera <sebastian.cabrera@gmail.com>
2020-04-05 20:22:48 -0300

    Actualizaciones

---
+++ operaciones/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('add/form_operaciones/', views.form_operaciones),
+    path('add/', views.alta_operaciones),
+    path('find/', views.form_buscar_operaciones),
+    path('modify/', views.gestionar_operaciones),
+    path('', views.operaciones),
+
+]
