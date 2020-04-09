4014a15ef08e2785723a03658325c9a65c5624d5
sebastian-cabrera <sebastian.cabrera@gmail.com>
2020-04-08 21:57:02 -0300

    Se corrige funcion de modificacion y de creacion de productos para ajustar a la salida del resultado. Se agrega columna Tipo a lista_productos.html.

--- productos/urls.py
+++ productos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add/form_productos/', views.form_productos),
+    path('form_productos/', views.form_productos),
     path('add/', views.alta_productos),
     path('find/', views.form_buscar_productos),
     path('modify/', views.gestionar_producto),
--- productos/views.py
+++ productos/views.py
@@ -32,19 +32,7 @@ def form_buscar_productos(request):
         #returing the form template
         template = loader.get_template('usuarios/lista_usuarios.html')
         return HttpResponse(template.render())
-"""
-@csrf_exempt
-def form_buscar_productos_eliminar(request):
-    if request.method == 'POST':
-        busqueda = request.POST.get('busqueda')
-        productos = Producto.objects.filter(Nombre__contains=busqueda)
-        return render (request, 'productos/lista_producto_eliminar.html', {'productos': productos })
-    else:
-        #if post request is not true
-        #returing the form template
-        template = loader.get_template('productos/lista_productos.html')
-        return HttpResponse(template.render())
-"""
+
 @csrf_exempt
 def form_productos(request):
     #if post request came
@@ -54,35 +42,9 @@ def form_productos(request):
         variedad = request.POST.get('variedad')
         procedencia = request.POST.get('procedencia')
         zafra = request.POST.get('zafra')
-
-        obj = Producto()
-        obj.Nombre = nombre
-        obj.Variedad = variedad
-        obj.Procedencia = procedencia
-        obj.Zafra = zafra
-        obj.save()
-
-        productos = Producto.objects.all()
-        return render (request, 'productos/lista_producto_modif.html', {'productos': productos})
-    else:
-        #if post request is not true
-        #returing the form template
-        template = loader.get_template('productos/form_gestion.html')
-        return HttpResponse(template.render())
-"""
-@csrf_exempt
-def modificar_producto(request):
-    #if post request came
-    if request.method == 'POST':
-        #getting values from post
-        prod_id = request.POST.get('id')
-        nombre = request.POST.get('nombre')
-        variedad = request.POST.get('variedad')
-        procedencia = request.POST.get('procedencia')
-        zafra = request.POST.get('zafra')
         tipo = request.POST.get('tipo')
 
-        obj = Producto.objects.get(id=prod_id)
+        obj = Producto()
         obj.Nombre = nombre
         obj.Variedad = variedad
         obj.Procedencia = procedencia
@@ -90,46 +52,16 @@ def modificar_producto(request):
         obj.Tipo = tipo
         obj.save()
 
-        productos = Producto.objects.all()
+        productos = {
+            nombre, variedad, procedencia, zafra, tipo
+        }
         return render (request, 'productos/lista_producto_modif.html', {'productos': productos})
     else:
         #if post request is not true
         #returing the form template
         template = loader.get_template('productos/form_gestion.html')
         return HttpResponse(template.render())
-"""
-###
-"""
-#
-#   Funcion para eliminacion de Productos
-#
 
-@csrf_exempt
-def eliminar_producto(request):
-    #if post request came
-
-    if request.method == 'POST':
-        #getting values from post
-        prod_id = request.POST.get('id')
-        nombre = request.POST.get('nombre')
-        variedad = request.POST.get('variedad')
-        procedencia = request.POST.get('procedencia')
-        zafra = request.POST.get('zafra')
-        tipo = request.POST.get('tipo')
-        producto = {
-            prod_id, nombre, variedad, procedencia, zafra, tipo,
-        }
-
-        obj = Producto.objects.get(id=prod_id)
-        obj.delete()
-
-        return render (request, 'productos/lista_producto_modif.html', {'producto': producto})
-    else:
-        #if post request is not true
-        #returing the form template
-        template = loader.get_template('productos/form_gestion.html')
-        return HttpResponse(template.render())
-"""
 
 
 ##############################################################################
@@ -149,6 +81,11 @@ def gestionar_producto(request):
         tipo = request.POST.get('tipo')
         accion = request.POST.get('accion')
 
+        productos = {
+            prod_id, nombre, variedad,
+            procedencia, zafra, tipo
+        }
+
         if accion == 'Modificar':
             obj = Producto.objects.get(id=prod_id)
             obj.Nombre = nombre
@@ -158,22 +95,14 @@ def gestionar_producto(request):
             obj.Tipo = tipo
             obj.save()
 
-            producto = {
-                obj.Nombre, obj.Variedad, obj.Procedencia, obj.Zafra, obj.Tipo
-            }
-
-            return render (request, 'productos/lista_producto_modif.html', {'producto': producto})
+            return render (request, 'productos/lista_producto_modif.html', {'productos': productos})
 
         elif accion == 'Borrar':
-
-            producto = {
-                prod_id, nombre, variedad, procedencia, zafra, tipo,
-            }
 
             obj = Producto.objects.get(id=prod_id)
             obj.delete()
 
-            return render (request, 'productos/lista_producto_modif.html', {'producto': producto})
+            return render (request, 'productos/lista_producto_modif.html', {'productos': productos})
         else:
             return render (request, 'productos/lista_productos.html')
     else:
